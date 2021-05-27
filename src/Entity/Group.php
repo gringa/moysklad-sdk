640a4bee1bbf6675ce5cb4d47dac8fe47ab6f6ef
gringa <gringa@gringa.me>
2021-05-28 01:52:24 +0500

    Поправленное описание

--- src/Entity/Group.php
+++ src/Entity/Group.php
@@ -5,6 +5,7 @@ namespace MoySkladSDK\Entity;
 /**
  * Class Group
  * Отдел
+ * @package MoySkladSDK\Entity
  * @author gringa <gringa@gringa.me>
  * @licence MIT
  * @link https://dev.moysklad.ru/doc/api/remap/1.2/dictionaries/#suschnosti-otdel
